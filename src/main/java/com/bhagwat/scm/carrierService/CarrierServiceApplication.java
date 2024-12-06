430ef0c7002726c80d613a303459b2172e0d309f
Prasannakrishna B <prasannakrishna.b@mphasis.com>
2024-12-06 14:48:22 +0530

    first add

---
+++ src/main/java/com/bhagwat/scm/carrierService/CarrierServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bhagwat.scm.carrierService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrierServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrierServiceApplication.class, args);
+	}
+
+}
